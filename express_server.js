7b46031ceb4ec273a94ec69d5a3b64e4af5e8bf0
Jorawer Grewal <joegrewal20@gmail.com>
2021-11-28 13:47:03 -0800

    Add generateRandomString function

--- express_server.js
+++ express_server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const length = characters.length;
+  for (let i = 0; i < 6; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * length));
+ }
+ return result;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -26,6 +36,11 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
