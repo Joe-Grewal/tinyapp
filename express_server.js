600c6ded4c0616a15f9d6a17f737967a0810ff52
Jorawer Grewal <joegrewal20@gmail.com>
2021-11-28 12:28:16 -0800

    Add body-parser

--- express_server.js
+++ express_server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 const app = express();
 const PORT = 8080; // default port 8080
 
