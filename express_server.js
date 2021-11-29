3fe7a91d589c42f0c02c8156bc48819d98c0db89
Jorawer Grewal <joegrewal20@gmail.com>
2021-11-28 23:18:25 -0800

    Install cookie-parser and add login form

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
+
 const app = express();
+app.use(cookieParser())
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
@@ -30,12 +33,18 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
+});
+
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body);
+  res.redirect("/urls");
 });
 
 app.post("/urls", (req, res) => {
@@ -59,7 +68,7 @@ app.post("/urls/:shortURL", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
